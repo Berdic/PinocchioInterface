ed98a6fa5d7886036cfff936de7a658fbfb8f04d
Berdic Nikola <berdicnikola@gmail.com>
2016-10-05 20:21:28 +0200

    Changed folder structure. Built applications are copied to root directory.

---
+++ Project/PinocchioInterface/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinocchioInterface.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        #region Properties
+        private ObservableCollection<RiggingModel> _riggingModels = new ObservableCollection<RiggingModel>();
+        public ObservableCollection<RiggingModel> RiggingModels
+        {
+            get
+            {
+                return _riggingModels;
+            }
+
+            set
+            {
+                _riggingModels = value;
+                NotifyPropertyChanged("RiggingModels");
+            }
+        }
+
+        private string _modelPath = "";
+
+        public string ModelPath
+        {
+            get { return _modelPath; }
+            set
+            {
+                _modelPath = value;
+                NotifyPropertyChanged("ModelPath");
+            }
+        }
+        #endregion
+
+        #region Functions
+        public void AutoRig()
+        {
+            string pinocchioPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "Pinocchio", "DemoUI.exe");
+            string motionFolder = System.IO.Path.Combine("Pinocchio", "motion");
+
+            foreach (RiggingModel model in RiggingModels)
+                Process.Start(pinocchioPath, model.GetCommandLineArguments(motionFolder));
+        }
+
+        public void AddModel(string path = null)
+        {
+            if (path == null)
+                RiggingModels.Add(new RiggingModel(ModelPath));
+            else
+                RiggingModels.Add(new RiggingModel(path));
+        }
+
+        public void RemoveModel(RiggingModel model)
+        {
+            RiggingModels.Remove(model);
+        }
+
+        public bool IsAlreadyInList(string path = null)
+        {
+            if (path == null)
+                return RiggingModels.Any(x => x.Path == ModelPath);
+            else
+                return RiggingModels.Any(x => x.Path == path);
+        }
+        #endregion
+
+        #region NotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
--- README.md
+++
@@ -1,7 +0,0 @@
-# PinocchioInterface
-This is the Windows Application which utilizes release of the Pinocchio library and
-gives it an interface for easier manipulation.
-The full technical description is given in Ilya Baran and Jovan Popovic,
-"Automatic Rigging and Animation of 3D Characters," SIGGRAPH 2007.
-The source can be found at http://www.mit.edu/~ibaran/autorig/code.html
-
