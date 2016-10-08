a5fb8750d0cd305cdf837949f193f2900c4621ac
Berdic Nikola <berdicnikola@gmail.com>
2016-10-08 23:56:12 +0200

    Changed interface.

--- Project/PostBuild.bat
+++ Project/PostBuild.bat
@@ -2,6 +2,7 @@ REM %1 = Solution Directory
 REM %2 = $(ConfigurationName) Debug/Release
 REM %3 = $(PlatformName) x86/x64
 
+
 IF  [%3] == x86 (
 
 	ECHO Using x86 configuration settings 
@@ -10,11 +11,12 @@ IF  [%3] == x86 (
 	xcopy "%1Resources\Pinocchio\" "%1PinocchioInterface\bin\%2\Pinocchio" /Y /E /R /I /D
 	xcopy "%1Resources\Pinocchio\*.*" "%1PinocchioInterface\bin\%2\Pinocchio" /Y /E
 	
-	ECHO Application x86 folder
 	
-	MD "%1..\Application %3"
-	xcopy "%1PinocchioInterface\bin\%3\%2\" "%1..\Application %3\"/Y /E /R /I /D
-	xcopy "%1PinocchioInterface\bin\%3\%2\*.*" "%1.\Application %3\"/Y /E
+		ECHO Application x86 folder
+		
+		MD "%1..\Application %3"
+		xcopy "%1PinocchioInterface\bin\%3\%2\" "%1..\Application %3\"/Y /E /R /I /D
+		xcopy "%1PinocchioInterface\bin\%3\%2\*.*" "%1.\Application %3\"/Y /E
 	
 ) ELSE (
 
@@ -24,9 +26,11 @@ IF  [%3] == x86 (
 	xcopy "%1Resources\Pinocchio\" "%1PinocchioInterface\bin\%3\%2\Pinocchio\" /Y /E /R /I /D
 	xcopy "%1Resources\Pinocchio\*.*" "%1PinocchioInterface\bin\%3\%2\Pinocchio\" /Y /E
 	
-	ECHO Application x64 folder
 	
-	MD "%1..\Application %3"
-	xcopy "%1PinocchioInterface\bin\%3\%2\" "%1..\Application %3\"/Y /E /R /I /D
-	xcopy "%1PinocchioInterface\bin\%3\%2\*.*" "%1..\Application %3\"/Y /E
+		ECHO Application x64 folder
+		
+		MD "%1..\Application %3"
+		xcopy "%1PinocchioInterface\bin\%3\%2\" "%1..\Application %3\"/Y /E /R /I /D
+		xcopy "%1PinocchioInterface\bin\%3\%2\*.*" "%1..\Application %3\"/Y /E
+	
 )
