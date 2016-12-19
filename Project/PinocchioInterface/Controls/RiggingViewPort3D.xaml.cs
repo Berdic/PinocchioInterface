b2deaf2f960b36afd44a3aa636c709b2793ba140
Berdic Nikola <berdicnikola@gmail.com>
2016-12-19 23:43:39 +0100

    Separated ViewPort3D in individual UserControl.

---
+++ Project/PinocchioInterface/Controls/RiggingViewPort3D.xaml.cs
@@ -0,0 +1,47 @@
+﻿using PinocchioInterface.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Media.Media3D;
+
+namespace PinocchioInterface.Controls
+{
+    /// <summary>
+    /// Interaction logic for RiggingViewPort3D.xaml
+    /// </summary>
+    public partial class RiggingViewPort3D : UserControl
+    {
+        
+        public RiggingViewPort3D()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        public RiggingModel RiggingModel
+        {
+            get { return (RiggingModel)GetValue(RiggingModelProperty); }
+            set { SetValue(RiggingModelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RiggingModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RiggingModelProperty =
+            DependencyProperty.Register("RiggingModel", typeof(RiggingModel), typeof(RiggingViewPort3D), new FrameworkPropertyMetadata(
+            null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        
+        
+    }
+}
